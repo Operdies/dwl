eb601c514e54c1490b53c20f86527fa1099e5122
alex <alexnlarsen@gmail.com>
2024-11-15 09:45:46 +0100

    fix startup build command

--- startup.sh
+++ startup.sh
@@ -39,7 +39,7 @@ do_launch() {
   export XDG_CURRENT_DESKTOP=dwl
   # Create a dbus session. This is primarily needed for kwallet
   export $(dbus-launch)
-  [ -x "$HOME/repos/dwl/release/dwl/dwl" ] || cd ~/repos/dwl && make release
+  [ -x "$HOME/repos/dwl/release/dwl/dwl" ] || make -C ~/repos/dwl release
   "$HOME/repos/dwl/release/dwl/dwl" -s "$0 startup"
 }
 
